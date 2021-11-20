9bf51b415736ffde2ae81898efb8f911068ae73f
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-20 12:39:49 +0530

    Update Setup

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
-import pathlib
-from setuptools import setup, find_packages
+import setuptools
 
 
-def requirements(file="requirements.txt") -> list:
+def requirements(file="requirements.txt"):
     if os.path.isfile(file):
         with open(file, encoding="utf-8") as r:
             return [i.strip() for i in r]
@@ -19,21 +18,22 @@ def readme(file="README.md"):
         return ""
 
 
-setup(
+setuptools.setup(
     name="",
     version="1.0.0",
-    author="FayasNoushad",
+    description="",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    description="",
-    license="MIT",
     url="https://github.com/FayasNoushad/",
+    license="MIT",
+    author="Fayas Noushad",
+    author_email="contact@fayas.me",
+    packages=setuptools.find_packages(),
+    install_requires=requirements(),
+    python_requires=">=3.6",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    packages=find_packages(),
-    install_requires=requirements(),
-    python_requires=">=3.6"
+    ]
 )
