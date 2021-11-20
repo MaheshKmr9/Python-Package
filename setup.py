e3fd976a386a6a5a49de6b8077953bd6f38c0ff4
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-20 13:37:04 +0530

    Add temporary documentation url

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
     project_urls={
         "Tracker": "https://github.com/FayasNoushad//issues",
         "Source": "https://github.com/FayasNoushad",
-        "Documentation": "",
+        "Documentation": "https://projects.fayas.me",
     },
     python_requires=">=3.6",
     packages=setuptools.find_packages(),
